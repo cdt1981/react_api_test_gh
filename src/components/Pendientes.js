e8df1aef993a5fe43fbe1cd278d5ef03d4556357
Carlos <carlos.tropiano@gmail.com>
2024-01-30 00:20:19 -0300

    organizé los divs que informan interlocutorios pendientes

--- src/components/Pendientes.js
+++ src/components/Pendientes.js
@@ -4,7 +4,7 @@ export default function Pendientes(props) {
 
     const [pendientes, setPendientes] = useState([]);
     const [isUpdated, setIsUpdated] = useState(true);
-    
+
     function obtenerRepetidos() {
         return fetch("https://script.google.com/macros/s/AKfycbyB9MzospzpnpHWEQerbLLmJM-oufC7GzIwC2vP0uL3rsEkdWwj6WF5iyv7ImnODHki/exec?action=obtenerRepetidos",
             { method: "GET" })
@@ -17,24 +17,29 @@ export default function Pendientes(props) {
                 console.log(pendientes)
             })
     }
-    
+
     useEffect(() => {
         obtenerRepetidos()
     }, [isUpdated])
 
     return (
-        <section className="d-flex flex-fill m-2 shadow bg-body-tertiary rounded border p-3">
-            <div className='border border-solid rounded shadow bg-warning' style={{fontSize: "2rem"}}>
-                Terminados: {pendientes.Terminado}
+        <section className="d-xl-flex justfy-content-center flex-wrap m-2 shadow bg-body-tertiary rounded border p-3 text-white">
+            <div className='row p-3 d-flex flex-fill'>
+                <div className='border border-solid rounded shadow bg-warning d-flex justify-content-center align-items-center mb-2' style={{ fontSize: "2rem" }}>
+                    <span> Terminados: </span><br></br>
+                    <span>{pendientes.Terminado}</span>
+                </div>
+                <div className='border border-solid rounded shadow bg-success d-flex justify-content-center align-items-center' style={{ fontSize: "2rem" }}>
+                    Circulando: {pendientes.Circulando}
+                </div>
             </div>
-            <div className='border border-solid rounded shadow bg-warning' style={{fontSize: "2rem"}}>
-                Circulando: {pendientes.Circulando}
-            </div>
-            <div className='border border-solid rounded shadow bg-warning' style={{fontSize: "2rem"}}>
-                Registrados: {pendientes.Registrado}
-            </div>
-            <div className='border border-solid rounded shadow bg-warning' style={{fontSize: "2rem"}}>
-                Pendientes: {pendientes.Pendiente}
+            <div className='row p-3 d-flex flex-fill'>
+                <div className='border border-solid rounded shadow bg-primary d-flex justify-content-center align-items-center mb-2' style={{ fontSize: "2rem" }}>
+                    Registrados: {pendientes.Registrado}
+                </div>
+                <div className='border border-solid rounded shadow bg-danger d-flex justify-content-center align-items-center' style={{ fontSize: "2rem" }}>
+                    Pendientes: {pendientes.Pendiente}
+                </div>
             </div>
         </section>
     )
